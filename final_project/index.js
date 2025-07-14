78514a61b3db667f4c8469273dfb140e1489c6b1
olky <sashakirichek@gmail.com>
2025-07-14 17:27:10 +0300

    correct files

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,32 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use(function auth(req, res, next) {
+    console.log(req.url);
+    next();
 });
- 
-const PORT =5000;
+
+app.use("/customer/auth/*", function auth(req, res, next) {
+    console.log(req.url);
+    if (!req.session.authorization) {
+        return res.status(403).json({ message: "User need to login first" })
+    }
+
+    jwt.verify(req.session.authorization['accessToken'], "secure-secret", (err, user) => {
+        if (err) {
+            return res.status(403).json({ message: "User not authenticated" });
+        }
+
+        req.user = user;
+        next();
+    });
+});
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,42 +2,123 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+let axios = require("axios");
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Check if both username and password are provided
+  if (username && password) {
+    // Check if the user does not already exist
+    if (!isValid(username)) {
+      // Add the new user to the users array
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registered. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+  // Return error if username or password is missing
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  return res.send(JSON.stringify(books, null, 2));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get('/isbn/:isbn', function (req, res) {
+  const result = books[req.params.isbn];
+
+  if (!result) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  return res.send(result);
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+  const result = Object.keys(books).filter(id => books[id].author === req.params.author).map(id => books[id]);
+
+  if (result.length === 0) {
+    return res.status(404).json({ message: "Books of this author are not found" });
+  }
+
+  return res.send(result);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  const result = Object.keys(books).filter(id => books[id].title === req.params.title).map(id => books[id]);
+
+  if (result.length === 0) {
+    return res.status(404).json({ message: "Books by this title are not found" });
+  }
+
+  return res.send(result);
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  const result = books[req.params.isbn];
+
+  if (!result) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  return res.send(result.reviews);
 });
 
 module.exports.general = public_users;
+
+
+const baseUrl = 'https://sashakiriche-5000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai';
+
+async function getAllBooks() {
+  try {
+    const response = await axios.get(baseUrl + '/');
+    const books = response.data;
+    console.log(JSON.stringify(books, null, 4));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getBooksByIsbn(isbn) {
+  try {
+    const response = await axios.get(baseUrl + '/isbn/' + encodeURIComponent(isbn));
+    const books = response.data;
+    console.log(JSON.stringify(books, null, 4));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+async function getBooksByAuthor(author) {
+  try {
+    const response = await axios.get(baseUrl + '/author/' + encodeURIComponent(author));
+    const books = response.data;
+    console.log(JSON.stringify(books, null, 4));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+async function getBooksByTitle(title) {
+  try {
+    const response = await axios.get(baseUrl + '/title/' + encodeURIComponent(title));
+    const books = response.data;
+    console.log(JSON.stringify(books, null, 4));
+  } catch (error) {
+    console.error(error);
+  }
+}
